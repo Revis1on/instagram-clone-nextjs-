76c5b9440e5eea33633848859dc4cbea501b01d5
Filip <filip22risteski@gmail.com>
2021-11-10 12:52:35 +0100

    added some stuff

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Instagram 2.0 Clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>This is starting point</h1>
+      {/* Header */}
+      {/* Мodul */}
+      {/* Feed */}
+    </div>
+  );
+}
